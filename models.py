c608843649d2309ad7a6f2de3f9f75517e32d351
Whising <86670009+Whisingdilli71@users.noreply.github.com>
2026-02-21 17:45:54 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import requests
 
-GOOGLE_API_KEY = "AIzaSyBaBufVHGUC9Gwgzw_sJRLzAyGJnaITnms"
+GOOGLE_API_KEY = "I USED THIS TO FIGURE OUT THE MODELS FOR MY KEY"
 url = f"https://generativelanguage.googleapis.com/v1/models?key={GOOGLE_API_KEY}"
 
 response = requests.get(url)
 print(response.json())
+
